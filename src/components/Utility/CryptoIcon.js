79a6d871e56940ba77afb003778095f0e8030e07
Borxes <yurasherman+github@gmail.com>
2019-09-06 17:46:26 +0300

    add Icons to the tag section

--- src/components/Utility/CryptoIcon.js
+++ src/components/Utility/CryptoIcon.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faNode, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
 
-import { Eth, Xtz } from 'react-cryptocoins'
+import { Eth, Xtz, Usdt } from 'react-cryptocoins'
 
 export default function CryptoIcon({ crypto }) {
-  console.log(crypto)
   switch (crypto) {
     case 'Ethereum':
       return <Eth />
     case 'Tezos':
       return <Xtz />
+    case 'Stablecoin':
+      return <Usdt />
+    case 'Twitter':
+      return <FontAwesomeIcon icon={faTwitter} />
+    case 'Bots':
+      return <FontAwesomeIcon icon={faRobot} />
+    case 'Node.js':
+      return <FontAwesomeIcon icon={faNode} />
+    case 'Javascript':
+      return <FontAwesomeIcon icon={faJs} />
+    case 'React':
+    case 'Gatsby':
+      return <FontAwesomeIcon icon={faReact} />
+
     default:
       return null
   }
---
+++ src/pages/articles/2019-09-05-Stablecoin-test.md
@@ -0,0 +1,12 @@
+---
+date: "2019-09-01"
+title: "stable tests coin"
+category: "Cryptocurrency"
+layout: post
+draft: false
+path: "/posts/stabletest"
+tags: ["Stablecoin"]
+description: "In this guide we will see how to execute Ethereum transaction from the client-side, i.e. from the browser, without third-party tools (e.g. Metamask). Namely, we will transfer ERC20 tokens from a wallet controlled by the web application to an arbitrary user's wallet."
+---
+
+test stablecoinz
